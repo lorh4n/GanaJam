4af6f52c82a70585dc4a10c504bc9da74bf3a389
Lorhan <gabriellorhan0@gmail.com>
2025-06-01 17:48:45 -0300

    Spawner colocado

--- snakewithrapadura/Scenes/spawner.gd
+++ snakewithrapadura/Scenes/spawner.gd
@@ -3,18 +3,42 @@ extends Node
 func _ready():
 	$"../AnimationPlayer".play("Spawnar")
 
-const BATERA = preload("res://Scenes/Object/batera.tscn")
-#const CAPIVARA = preload("res://Scenes/Capivara.tscn")
+const batera = preload("res://Scenes/Object/batera.tscn")
+const gamuto = preload("res://Scenes/Object/gamuto.tscn")
+const capivara = preload("res://Scenes/Object/Capivara.tscn")
+const mistura = preload("res://Scenes/Object/mistura.tscn")
+const brasil = preload("res://Scenes/Object/brasil.tscn")
+const guitarra = preload("res://Scenes/Object/Guitarra.tscn")
+const pao = preload("res://Scenes/Object/pao_mortadela.tscn")
+const rede = preload("res://Scenes/Object/rede.tscn")
+const sakura = preload("res://Scenes/Object/sakura.tscn")
+const saque = preload("res://Scenes/Object/Saque.tscn")
+const shibainu = preload("res://Scenes/Object/shibainu.tscn")
+const violao = preload("res://Scenes/Object/violao.tscn")
+const zabumba = preload("res://Scenes/Object/zabumba.tscn")
+
+
 
 var positions = [
-	Vector2(960, 370),
-	Vector2(960, 400),
-	Vector2(960, 540)
+	Vector2(960, 360),
+	Vector2(960, 420),
+	Vector2(960, 480)
 ]
 
 var scenes = {
-	"batera": BATERA,
-	#"capivara": CAPIVARA
+	"batera" : batera,
+	"gamuto" : gamuto,
+	"capivara" : capivara,
+	"mistura" : mistura,
+	"brasil" : brasil,
+	"guitarra" : guitarra,
+	"pao" : pao,
+	"rede" : rede,
+	"sakura" : sakura,
+	"saque" : saque,
+	"shibainu" : shibainu,
+	"violao" : violao,
+	"zabumba" : zabumba
 }
 
 func spawnar(object_name: String, skip_position: int) -> void:
