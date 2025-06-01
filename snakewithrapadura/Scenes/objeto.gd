58acdaacbf8abec31e699911c34bb3d5ffd9fc2e
Lorhan <gabriellorhan0@gmail.com>
2025-06-01 10:22:41 -0300

    History altered

--- snakewithrapadura/Scenes/objeto.gd
+++ snakewithrapadura/Scenes/objeto.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 50000.0
+const SPEED = 1000.0
 var direction = -1
 
 func _physics_process(delta: float) -> void:
-	velocity.x = direction * SPEED * delta
+	velocity.x = direction * SPEED
 
 	move_and_slide()
