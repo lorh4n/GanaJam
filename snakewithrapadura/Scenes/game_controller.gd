9e82547c2f4e1c98c29e300f81918aa272bc08ee
Felipe <fpachecomanoel@uol.com.br>
2025-06-01 18:14:35 -0300

    finished game and added web export

--- snakewithrapadura/Scenes/game_controller.gd
+++ snakewithrapadura/Scenes/game_controller.gd
@@ -3,9 +3,14 @@ extends Node
 var music_position=0
 var music=preload("res://sound/MusicaPrincipal.mp3")
 var inverted_music=preload("res://sound/MusicaPrincipal_Invertido.mp3")
+var revive_music=preload("res://sound/RefraoInicio.mp3")
 var inverted=false
+var reviving=false
 func get_music_time():
-	return $AudioStreamPlayer.get_playback_position()
+	if not inverted:
+		return $AudioStreamPlayer.get_playback_position()
+	else:
+		return $AudioStreamPlayer.stream.get_length()-$AudioStreamPlayer.get_playback_position()
 
 func get_music_length():
 	return $AudioStreamPlayer.stream.get_length()
@@ -13,20 +18,26 @@ func get_music_length():
 func pause_music():
 	music_position=$AudioStreamPlayer.get_playback_position()
 	$AudioStreamPlayer.stop()
+	$AudioStreamPlayer.stream=revive_music
+	$AudioStreamPlayer.play()
+	reviving=true
+	$"../AnimationPlayer".stop()
+	for i in $"../Spawner".get_children():
+		i.queue_free()
 	
 func invert_music():
+	reviving=false
 	$AudioStreamPlayer.stream=inverted_music
 	$AudioStreamPlayer.play(inverted_music.get_length()-music_position)
 	inverted=true
 	get_parent().get_parent().material.set_shader_parameter("on",1)
-	$AnimationPlayer.stop()
-	#for i in $"../Spawner".get_children():
-		#i.queue_free()
+	
 
 func revert_music():
 	$AudioStreamPlayer.stream=music
 	$AudioStreamPlayer.play()
 	inverted=false
+	$"../AnimationPlayer".stop()
 
 func _on_audio_stream_player_finished() -> void:
 	if inverted:
@@ -36,3 +47,5 @@ func _on_audio_stream_player_finished() -> void:
 		$"../Player".visible=true
 		$"../Player".disabled=false
 		revert_music()
+	elif not reviving:
+		get_tree().change_scene_to_file("res://Scenes/ganhou.tscn")
